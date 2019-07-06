be45b3399770c3adb85d042fe3069c8026fde5b2
Tanaka Takayuki <ta@ttdyna.home.jp>
2019-07-06 11:34:35 +0900

    MyFtpを削除。ヘッダーにGithubアイコンを追加。

--- closed/php/header.php
+++ closed/php/header.php
@@ -4,6 +4,7 @@
             <h1><a href="index.php"><img src="images/title16.png" alt="<?php print($siteTitle); ?>"></a></h1>
             <div class="menubutton"><img src="images/menubutton.svg"></div>
             <div class="twitter_main_button">
+                <a href="https://github.com/aharotias2/" target="_blank"><img src="images/GitHub-Mark-Light-32px.png"></a>
                 <a href="https://twitter.com/goodslacktanaka" target="_blank"><img src="images/twitter_main_button_flat.png"></a>
                 <a href="https://gorigorilinux.net/rss.xml" target="_blank"><img src="images/rss-icon.png"></a>
             </div>
